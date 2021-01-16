56a93ecfb930ad513df1d62a0165710d53fd5fe5
aindriu80 <aindriu80@gmail.com>
2021-01-16 16:37:26 +0000

    Check out - Initial Stepper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
-import { Products, Navbar, Cart } from "./components";
+import { Products, Navbar, Cart, Checkout } from "./components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -14,8 +14,7 @@ const App = () => {
   };
 
   const fetchCart = async () => {
-    const cart = await commerce.cart.retrieve();
-    setCart(cart);
+    setCart(await commerce.cart.retrieve());
   };
 
   const handleAddToCart = async (productId, quantity) => {
@@ -62,6 +61,9 @@ const App = () => {
               handleRemoveFromCart={handleRemoveFromCart}
               handleEmptyCart={handleEmptyCart}
             />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout />
           </Route>
         </Switch>
       </div>
