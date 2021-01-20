16996c5acad4c5eaf895e34bccb244d68874ca82
aindriu80 <aindriu80@gmail.com>
2021-01-20 16:41:09 +0000

    Stepper - Review of order

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -30,7 +30,6 @@ const Checkout = ({ cart }) => {
         const token = await commerce.checkout.generateToken(cart.id, {
           type: "cart",
         });
-        console.log(token);
         setCheckoutToken(token);
       } catch (error) {}
     };
@@ -52,7 +51,10 @@ const Checkout = ({ cart }) => {
     activeStep === 0 ? (
       <AddressForm checkoutToken={checkoutToken} next={next} />
     ) : (
-      <PaymentForm />
+      <PaymentForm
+        shipppingData={shipppingData}
+        checkoutToken={checkoutToken}
+      />
     );
   return (
     <>
